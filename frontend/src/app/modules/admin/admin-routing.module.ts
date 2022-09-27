ae51017cc1b10105004416a97c2e45b370ad927f
darckdreck <darckdreck@gmail.com>
2022-09-27 19:33:40 -0400

    componente movies para crud creado cc

--- frontend/src/app/modules/admin/admin-routing.module.ts
+++ frontend/src/app/modules/admin/admin-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
+import { MoviesComponent } from './pages/movies/movies.component';
 
-const routes: Routes = [{ path: '', component: AdminComponent }];
+const routes: Routes = [
+  { path: '', component: AdminComponent },
+  { path: 'movies', component: MoviesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AdminRoutingModule { }
+export class AdminRoutingModule {}
--- frontend/src/app/modules/admin/admin.module.ts
+++ frontend/src/app/modules/admin/admin.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
+import { MoviesComponent } from './pages/movies/movies.component';
 
 
 @NgModule({
   declarations: [
-    AdminComponent
+    AdminComponent,
+    MoviesComponent
   ],
   imports: [
     CommonModule,
