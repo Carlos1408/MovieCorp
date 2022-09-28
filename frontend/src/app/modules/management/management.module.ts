a6f3cbea93e722c2a2731e841aeaa4e3e0055036
darckdreck <darckdreck@gmail.com>
2022-09-28 11:00:04 -0400

    gui de crud administradores creado con modulo management para gerente cc

--- frontend/src/app/modules/management/management.module.ts
+++ frontend/src/app/modules/management/management.module.ts
@@ -3,15 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { ManagementRoutingModule } from './management-routing.module';
 import { ManagementComponent } from './management.component';
+import { UsersComponent } from './pages/users/users.component';
+import { UserFormComponent } from './pages/users/components/user-form/user-form.component';
+import { UserTableComponent } from './pages/users/components/user-table/user-table.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    ManagementComponent
+    ManagementComponent,
+    UsersComponent,
+    UserFormComponent,
+    UserTableComponent
   ],
   imports: [
     CommonModule,
-    ManagementRoutingModule
+    ManagementRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class ManagementModule { }
