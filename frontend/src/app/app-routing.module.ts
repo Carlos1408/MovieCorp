be934c0566d8929980f7fab4af58724c37ef7a08
darckdreck <darckdreck@gmail.com>
2022-09-28 10:31:32 -0400

    modulo para gerente creado cc

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'admin', loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule) }];
+const routes: Routes = [
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./modules/admin/admin.module').then((m) => m.AdminModule),
+  },
+  {
+    path: 'management',
+    loadChildren: () =>
+      import('./modules/management/management.module').then(
+        (m) => m.ManagementModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
