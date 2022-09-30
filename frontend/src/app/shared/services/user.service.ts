596fb483aa4431e48b78aee53a3b53a797ccc389
cclarev <cclarev@est.emi.edu.bo>
2022-09-29 21:35:43 -0400

    interface y service de usuarios creados con metodos http cc

---
+++ frontend/src/app/shared/services/user.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { User } from '../interfaces/user';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private URL_API = `${environment.API_BASE_URL}/api/v1/users`;
+
+  constructor(private http: HttpClient) {}
+
+  getAllUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.URL_API);
+  }
+
+  getUser(_id: string): Observable<User> {
+    return this.http.get<User>(`${this.URL_API}/${_id}`);
+  }
+
+  createUser(user: User): Observable<User> {
+    return this.http.post<User>(this.URL_API, user);
+  }
+
+  updateUser(user: User): Observable<User> {
+    return this.http.put<User>(`${this.URL_API}/${user._id}`, user);
+  }
+
+  deleteUser(_id: string): Observable<User> {
+    return this.http.delete<User>(`${this.URL_API}/${_id}`);
+  }
+}
