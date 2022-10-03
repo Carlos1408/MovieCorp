59cd61c6114ac4d1e0051780ce177e729f982c80
darckdreck <darckdreck@gmail.com>
2022-10-03 10:40:24 -0400

    primeng migracion de aplicacion cc

---
+++ frontend/src/app/primeng.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// primeng modules imports, debe estar importado y declarado en exports
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: []
+})
+export class PrimengModule { }
