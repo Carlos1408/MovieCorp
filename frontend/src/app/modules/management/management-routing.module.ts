84ce8172e439a12c1073e4a20339a66e83bb7686
Vitor-95 <vcp256@gmail.com>
2022-10-17 20:41:33 -0400

    HU29 GUI perfil de gerente

--- frontend/src/app/modules/management/management-routing.module.ts
+++ frontend/src/app/modules/management/management-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ManagementComponent } from './management.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 import { UsersComponent } from './pages/users/users.component';
 
 const routes: Routes = [
   { path: '', component: ManagementComponent },
   { path: 'users', component: UsersComponent },
+  { path: 'profile', component: ProfileComponent}
 ];
 
 @NgModule({
