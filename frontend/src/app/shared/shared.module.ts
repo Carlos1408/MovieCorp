cddbb90922f1850ef63720e89d0b55597c1426f9
cclarev <cclarev@est.emi.edu.bo>
2022-10-18 00:02:48 -0400

    import de primeng a shared cc

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OptionCardComponent } from './components/option-card/option-card.component';
+import { PrimengModule } from './primeng.module';
 
 @NgModule({
   declarations: [OptionCardComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, PrimengModule],
   exports: [OptionCardComponent],
 })
 export class SharedModule {}
