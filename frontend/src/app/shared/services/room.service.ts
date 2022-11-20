8bd934dff2e738f2c7ad8f894c8441bf51d9773b
cclarev <cclarev@est.emi.edu.bo>
2022-11-20 02:08:58 -0400

    validacion de horario en creacion de funcion cc

--- frontend/src/app/shared/services/room.service.ts
+++ frontend/src/app/shared/services/room.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Room } from '../interfaces/room';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
@@ -38,5 +38,39 @@ export class RoomService {
 
   deleteRoom(_id: string): Observable<Room> {
     return this.http.delete<Room>(`${this.URL_API}/${_id}`);
+  }
+
+  isTimeRangeAvailable(
+    room: Room | null,
+    from: Date,
+    to: Date
+  ): Observable<boolean> {
+    let result = true;
+    if (room) {
+      room.timeRanges.forEach((tr) => {
+        const d: Date = new Date(tr.from);
+        const h: Date = new Date(tr.to);
+        const i: Date = from;
+        const f: Date = to;
+        if (i.getHours() > h.getHours() && f.getHours() > h.getHours()) {
+          if (!(i.getHours() > d.getHours() && f.getHours() > d.getHours()))
+            result = false;
+        } else if (i.getHours() < d.getHours() && f.getHours() < d.getHours()) {
+          if (!(i.getHours() < h.getHours() && f.getHours() < h.getHours()))
+            result = false;
+        } else if (
+          i.getHours() < d.getHours() &&
+          f.getHours() === d.getHours()
+        ) {
+          if (!(f.getMinutes() < d.getMinutes())) result = false;
+        } else if (
+          i.getHours() === h.getHours() &&
+          f.getHours() > h.getHours()
+        ) {
+          if (!(i.getMinutes() > h.getMinutes())) result = false;
+        } else result = false;
+      });
+    }
+    return of(result);
   }
 }
