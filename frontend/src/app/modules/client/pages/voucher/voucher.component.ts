332155d7a6f184b04f7f3f652b8bad4cb03a6154
darckdreck <darckdreck@gmail.com>
2022-11-23 07:37:06 -0400

    gui de ingreso de datos cc

--- frontend/src/app/modules/client/pages/voucher/voucher.component.ts
+++ frontend/src/app/modules/client/pages/voucher/voucher.component.ts
@@ -1,23 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-voucher',
   templateUrl: './voucher.component.html',
-  styleUrls: ['./voucher.component.scss']
+  styleUrls: ['./voucher.component.scss'],
 })
 export class VoucherComponent implements OnInit {
-  
   voucherForm: FormGroup = new FormGroup({
-    names: new FormControl(''),
-    ci: new FormControl(''),
-    email: new FormControl(''),
-
+    names: new FormControl('', Validators.required),
+    ci: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
   });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
